6b440248c87bc2d9197370731c653c098af32397
Andre Luiz Belmonte Ferreira <andre.ferreira@cinpal.com>
2023-09-11 15:21:57 -0300

    criando a primeira script

---
+++ amass.sh
@@ -0,0 +1,7 @@
+apt install amass
+
+## para descobrir um dominio ou extensões de uma empresa utilizamos um comando.
+## Por exemplo o whois que nos mostra todos os IP's
+## -org é a organização que quer dar uma olhada.
+amass intel .org "Facebook Inc." whois
+
